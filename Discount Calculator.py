7b9cea3c4c9a3a34892fc39fd76bfb6dcff1bd05
tejasgund <89707440+tejasgund@users.noreply.github.com>
2023-06-07 18:09:24 +0530

    Update Discount Calculator.py

--- Discount Calculator.py
+++ Discount Calculator.py
@@ -1,13 +1,15 @@
 import tkinter as tk
 from tkinter import ttk
 room=tk.Tk()
-Frame1=tk.LabelFrame(room,text="Enter A Value",background='red')
-Frame1.place(x=20,y=10,relwidth=1,height=300)
+room.geometry("900x900")
+
+Frame1=tk.LabelFrame(room,text="Enter A Value")
 Frame2=tk.LabelFrame(room,text="Enter A Value")
-Frame2.place(x=20,y=500,relwidth=1)
+
 
 value1=tk.StringVar()
 value2=tk.StringVar()
+right_wrong=0
 
 name1=tk.Label(Frame1,text="Enter A Product Mrp Value")
 name2=tk.Label(Frame1,text="Enter A Product Actual Value")
@@ -19,7 +21,8 @@ name22=tk.Label(Frame2,text="Discount In Rupees")
 V1=tk.Label(Frame2,textvariable=value1)
 V2=tk.Label(Frame2,textvariable=value2)
 
-
+Frame1.place(x=20,y=10,relwidth=1,height=300)
+Frame2.place(x=20,y=500,relwidth=1)
 l1.grid(row=0,column=1)
 l2.grid(row=1,column=1)
 name1.grid(row=0,column=0)
@@ -32,16 +35,39 @@ V2.grid(row=1,column=1)
 l1,l2,name1,name2.grid(pady=20,padx=20,ipadx=20,ipady=20)
 V1,V2,name11,name22.grid(pady=20,padx=20,ipadx=20,ipady=20)
 
-
+#Design
+def normal():
+    room.configure(background="black")
+    f1_bg="red"
+    f2_bg="lightblue"
+    f1_font=("Arial", 25)
+    f2_font=("Arial", 25)
+    labels_font=("Arial", 15)
+    entry_fonts=("Arial", 15)
+    name1.configure(background=f1_bg,font=labels_font)
+    name2.configure(background=f1_bg,font=labels_font)
+    name11.configure(background=f1_bg,font=labels_font)
+    name22.configure(background=f1_bg,font=labels_font)
+    l1.configure(font=entry_fonts)
+    l2.configure(font=entry_fonts)
+    V1.configure(font=entry_fonts)
+    V2.configure(font=entry_fonts)
+    Frame1.configure(background=f1_bg,font=f1_font)
+    Frame2.configure(background=f1_bg,font=f2_font)
+normal()
 
 def vv(event=None):
    try:
       s = int(l1.get()) - int(l2.get())
-      value2.set("Differnce :\t"+str(s)+"")
+      value2.set(" :\t"+str(s)+"")
       s1 = int(s) / int(l1.get()) * 100
-      value1.set("Discount :\t" + str(int(s1))+" %")
+      value1.set(" :\t" + str(int(s1))+" %")
    except:
-      pass
+        right_wrong=1
+        room.configure(background="black")
+
+    
+
 
 
 
